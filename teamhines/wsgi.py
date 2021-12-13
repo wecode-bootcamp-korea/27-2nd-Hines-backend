2ebf13462c0fa96d30a90ae83d132a0d6117e2c1
Dowon Jang <rosewoodowon@gmail.com>
2021-12-13 17:41:43 +0900

    Add: project initial setting

---
+++ teamhines/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for teamhines project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teamhines.settings')
+
+application = get_wsgi_application()
