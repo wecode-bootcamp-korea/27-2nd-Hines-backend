cecc049e7b1eb9a94f16250fdee3d62bc63adde7
Dowon Jang <rosewoodowon@gmail.com>
2021-12-14 16:30:30 +0900

    Add: models.py 작성

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db       import models
+from core.models     import TimeStampModel
+from users.models    import User
+from products.models import Product
 
-# Create your models here.
+class Cart(TimeStampModel):
+    quantity = models.IntegerField(default=0)
+    user     = models.ForeignKey(User, on_delete=models.CASCADE)
+    product  = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'carts'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db   import models
+from core.models import TimeStampModel
 
-# Create your models here.
+class User(TimeStampModel):
+    name     = models.CharField(max_length=45)
+    kakao_id = models.CharField(max_length=45)
+    email    = models.EmailField(unique=True)
+
+    class Meta:
+        db_table = 'users'
