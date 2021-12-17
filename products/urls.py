81d2732b735651d67a43195649fee2d874738d32
Dowon Jang <rosewoodowon@gmail.com>
2021-12-17 20:35:44 +0900

    Modify: class명 복수형으로 수정, 유닛테스트 프린트문 삭제, conflict해결, try except로 수정, value error 테스트코드 추가, 카테고리뷰 카테고리만 반환하도록 수정

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
 from .views      import ProductListView, ProductDetailView
+from .views      import ProductListView, CategoriesView
 
 urlpatterns=[
     path('', include([
         path('', ProductListView.as_view()),
     ])),
     path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/categories', CategoriesView.as_view()),
 ]
