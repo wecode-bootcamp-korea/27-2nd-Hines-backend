c6f049e4880cc86e283d623ea85aa45d28af2000
Dowon Jang <rosewoodowon@gmail.com>
2021-12-20 15:26:16 +0900

    Modify: 프로덕트 리스트 by category_id 기능추가

--- products/views.py
+++ products/views.py
@@ -8,6 +8,7 @@ from .models          import Menu, Category, Product, SubCategory
 
 class ProductListView(View):
     def get(self, request):
+        category_id     = request.GET.get('category_id')
         sub_category_id = request.GET.get('sub_category_id')
         search_keyword  = request.GET.get('search')
         order_method    = request.GET.get('sort_method', 'created_at')
@@ -24,6 +25,12 @@ class ProductListView(View):
             q &= Q(name__icontains=search_keyword)|Q(brand__icontains=search_keyword)|Q(description__icontains=search_keyword)
 
         products = Product.objects.filter(q).order_by(order_method)[offset:offset+limit]
+
+        if category_id:
+            q &= Q(category_id=category_id)
+
+            products = Product.objects.filter(Q(sub_category_id__category_id=category_id)).order_by(order_method)[offset:offset+limit]
+            
         results = [{
             'id'                  : product.id,
             'name'                : product.name,
