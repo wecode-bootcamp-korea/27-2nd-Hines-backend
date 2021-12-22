ce3a13bf4cb30de9c0ab9c0a976213b9db092268
이유진 <youjin@iyujin-ui-MacBookAir.local>
2021-12-22 21:35:39 +0900

    Modified :리뷰 반영, ReviewView unit test 작성
    -리뷰 반영, unit test 수정전
    -review unit test 수정 중
    -review unit test 수정완료
    -pr 수정(INVALID_PRODUCT_ID)
    -filter 리뷰반영

--- products/tests.py
+++ products/tests.py
@@ -1,9 +1,10 @@
-import json
+import json, jwt
 
 from django.http import response
 from django.test import TestCase, Client
 
-from .models import *
+from .models     import *
+from my_settings import SECRET_KEY, ALGORITHM
 
 class ProductsAppTest(TestCase):
     def setUp(self):
@@ -209,7 +210,7 @@ class ProductsAppTest(TestCase):
 class ProductDetailTest(TestCase):
     def setUp(self):
         self.client = Client()
-
+        
         Menu.objects.create(
             id   = 1,
             name = '홈'
@@ -220,7 +221,7 @@ class ProductDetailTest(TestCase):
             name    = '옷',
             menu_id = 1
         )
-       
+        
         SubCategory.objects.create(
             id          = 1,
             name        = '상의',
@@ -230,17 +231,17 @@ class ProductDetailTest(TestCase):
         Product.objects.create(
             id                  = 1,
             name                = '상품1',
-            price               = '1111111.00',
+            price               = 1111111.00,
             brand               = 'zara',
             description         = '상품1설명입니다',
             thumbnail_image_url = 'http:상품1썸네일이미지',
             sub_category_id     = 1 
         )
-       
+        
         Product.objects.create(
             id                  = 2,
             name                = '상품2',
-            price               = '222222.00',
+            price               = 222222.00,
             brand               = 'zara2',
             description         = '상품2설명입니다',
             thumbnail_image_url = 'http:상품2썸네일이미지',
@@ -250,17 +251,17 @@ class ProductDetailTest(TestCase):
         Product.objects.create(
             id                  = 3,
             name                = '상품3',
-            price               = '333333.00',
+            price               = 333333.00,
             brand               = 'zara3',
             description         = '상품3설명입니다',
             thumbnail_image_url = 'http:상품3썸네일이미지',
             sub_category_id     = 1
         )  
-        
+
         Product.objects.create(
             id                  = 4,
             name                = '상품4',
-            price               = '444444.00',
+            price               = 444444.00,
             brand               = 'zara4',
             description         = '상품4설명입니다',
             thumbnail_image_url = 'http:상품4썸네일이미지',
@@ -286,48 +287,58 @@ class ProductDetailTest(TestCase):
             image_url  = 'http:상품3이미지',
             product_id = 3
         )
-
+        
         Image.objects.create(
             image_url  = 'http:상품4이미지',
             product_id = 4
         )
         
     def tearDown(self):
-        Product.objects.all().delete() 
+        Menu.objects.all().delete()
+        SubCategory.objects.all().delete()
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+        Image.objects.all().delete()
         
-    def test_productdetailview_get_success(self):
+    def test_product_detail_view_get_success(self):
         client   = Client()
         response = client.get('/products/1')
         self.assertEqual(response.json(),
             {
-                'message' : {
-                        'name'                : '상품1',
-                        'price'               : '1111111.00',
-                        'brand'               : 'zara',
-                        'description'         : '상품1설명입니다',
-                        'thumbnail_image_url' : 'http:상품1썸네일이미지',
-                        'image_url'           : ['http:상품1이미지', 'http:상품5이미지']
+                'result' : {
+                    'category_name'       : '옷',
+                    'sub_category_name'   : '상의',
+                    'product_name'        : '상품1',
+                    'price'               : 1111111,
+                    'brand'               : 'zara',
+                    'description'         : '상품1설명입니다',
+                    'thumbnail_image_url' : 'http:상품1썸네일이미지',
+                    'image_url'           : ['http:상품1이미지', 'http:상품5이미지']
                 }
             }
-        )
+        )  
 
         self.assertEqual(response.status_code, 200)
 
-    def test_productdetailview_get_doesnotexist(self):
+    def test_product_detail_view_get_doesnotexist(self):
         client   = Client()
         response = client.get('/products/5')
         self.assertEqual(response.json(),
-            {'message': 'PRODUCT_DOESNOT_EXIST'}
+            {
+                'message': 'PRODUCT_DOESNOT_EXIST'
+            }
         )
         self.assertEqual(response.status_code, 400)
 
 class ReviewViewTest(TestCase):
     def setUp(self):
+        self.hines_token = jwt.encode({'id':1}, SECRET_KEY, ALGORITHM)
+        
         Menu.objects.create(
             id   = 1,
             name = '홈'
         )
-
+      
         Category.objects.create(
             id      = 1,
             name    = '옷',
@@ -339,68 +350,68 @@ class ReviewViewTest(TestCase):
             name        = '상의',
             category_id = 1
         )
-
+       
         Product.objects.create(
             id                  = 1,
             name                = '상품1',
-            price               = '1111111.00',
+            price               = 1111111.00,
             brand               = 'zara',
             description         = '상품1설명입니다',
             thumbnail_image_url = 'http:상품1썸네일이미지',
             sub_category_id     = 1 
         )
-
+        
         Product.objects.create(
             id                  = 2,
             name                = '상품2',
-            price               = '222222.00',
+            price               = 222222.00,
             brand               = 'zara2',
             description         = '상품2설명입니다',
             thumbnail_image_url = 'http:상품2썸네일이미지',
             sub_category_id     = 1
         )  
-
+        
         Product.objects.create(
             id                  = 3,
             name                = '상품3',
-            price               = '333333.00',
+            price               = 333333.00,
             brand               = 'zara3',
             description         = '상품3설명입니다',
             thumbnail_image_url = 'http:상품3썸네일이미지',
             sub_category_id     = 1
         )  
-
+        
         Image.objects.create(
             id         = 1,
             image_url  = 'http:상품1이미지',
             product_id = 1
         )
-
+        
         Image.objects.create(
             id         = 2,
             image_url  = 'http:상품5이미지',
             product_id = 1
         )
-
+        
         Image.objects.create(
             id         = 3,
             image_url  = 'http:상품2이미지',
             product_id = 2
         )
-
+        
         Image.objects.create(
             id         = 4,
             image_url  = 'http:상품3이미지',
             product_id = 3
         )
-
+        
         User.objects.create(
             id       = 1,
             name     = '전원우',
             kakao_id = '1',
             email    = 'wonwoo@gmial.com'
         )
-
+        
     def tearDown(self):
         Menu.objects.all().delete()
         Category.objects.all().delete()
@@ -409,80 +420,73 @@ class ReviewViewTest(TestCase):
         User.objects.all().delete()
 
     def test_review_view_test_post_success(self):
-        client = Client()
-
+        client  = Client()
+        headers = {'HTTP_Authorization' : self.hines_token}
+        
         result = {
-            'kakao_id'   : 1, 
-            'product_id' : 1,
-            'content'    : 'test111',
-            'image_url'  : 'test1'
+            'content'   : 'test111',
+            'image_url' : 'test1'
         }
+        
+        response = client.post('/products/1/review', json.dumps(result), content_type='application/json', **headers)
 
-        response = client.post('/products/review', json.dumps(result), content_type='application/json')
-
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json(),
             {
                 'message':'SUCCESS'
-            }   
+            }
         )
 
     def test_review_view_test_post_key_error(self):
-        client = Client()
+        client  = Client()
+        headers = {'HTTP_Authorization' : self.hines_token}
 
         result = {
-            'id'         : 1, 
-            'product_id' : 1,
-            'content'    : 'test111',
-            'image_url'  : 'test1'
+            'con  ' : 'test111',
+            'image' : 'test1'
         }
-
-        response = client.post('/products/review', json.dumps(result), content_type='application/json')
-
-        self. assertEqual(response.status_code, 400)
-
+    
+        response = client.post('/products/1/review', json.dumps(result), content_type='application/json', **headers)
+        
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
             {
                 'message':'KEY_ERROR'
             }
         )
 
-    def test_review_view_test_post_unauthorized_user(self):
-        client = Client()
+    def test_review_view_test_post_key_error(self):
+        client  = Client()
+        headers = {'HTTP_Authorization' : self.hines_token}
 
         result = {
-            'kakao_id'   : 2, 
-            'product_id' : 1,
-            'content'    : 'test111',
-            'image_url'  : 'test1'
+            'con'       : 'test111',
+            'image_url' : 'test1'
         }
-
-        response = client.post('/products/review', json.dumps(result), content_type='application/json')
-
-        self.assertEqual(response.status_code, 401)
-
+    
+        response = client.post('/products/1/review', json.dumps(result), content_type='application/json', **headers)
+        
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
             {
-                'message':'UNAUTHORIZED_USER'
-            }   
+                'message':'KEY_ERROR'
+            }
         )
 
-    def test_review_view_test_post_no_product(self):
-        client = Client()
+    def test_review_view_test_post_invalid_product(self):
+        client  = Client()
+        headers = {'HTTP_Authorization' : self.hines_token}
 
         result = {
-            'kakao_id'   : 1, 
-            'product_id' : 5,
-            'content'    : 'test111',
-            'image_url'  : 'test1'
+            'content'   : 'test111',
+            'image_url' : 'test1'
         }
-
-        response = client.post('/products/review', json.dumps(result), content_type='application/json')
-
+    
+        response = client.post('/products/10/review', json.dumps(result), content_type='application/json', **headers)
+        
         self.assertEqual(response.status_code, 400)
-
         self.assertEqual(response.json(),
             {
-                'message':'NO_PRODUCT'
-            } 
+                'message':'INAVID_PRODUCT_ID'
+            }
         )
