0795e5c96599ce702caa65b7fb6778568fa5759f
Dowon Jang <rosewoodowon@gmail.com>
2021-12-23 16:32:20 +0900

    Modify: CartView수정, patch 에러추가, urls.py 수정

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views      import CartView
 
 urlpatterns=[
-    path('/<int:cart_id>', CartView.as_view()),
+    path('/<int:product_id>', CartView.as_view()),
     path('', CartView.as_view())
 ]
